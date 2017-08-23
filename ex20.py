87186092f8e526eee2f96e88ae6766a0707a431d
Andrey <andryush333@gmail.com>
2017-08-23 13:07:18 +0400

    adding ex20.py and files

---
+++ ex20.py
@@ -0,0 +1,37 @@
+from sys import argv
+script, input_file = argv
+
+def print_all(f): # f is variable and whatever we can pass
+    print(f.read()) # reading file
+
+def rewind(f):
+    f.seek(0) # seek is for move coursore to begin of the file
+
+def print_a_line(line_count, f): #line_count is also variable and anything other can pass there
+    print(line_count, f.readline()) #readline is reading current line, when print is nothing it means end of file
+
+current_file = open(input_file)
+
+print('First lets print whole file:\n')
+print_all(current_file)
+
+print("Now let's rewind, kind of like a tape.")
+rewind(current_file)
+
+print("Let's print three lines:")
+
+current_line = 1
+print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
+
+current_line += 1
+print_a_line(current_line, current_file)
+
+current_line += 1
+print_a_line(current_line, current_file)
+
+rewind(current_file)
+current_line += 1
+print_a_line(current_line, current_file)
