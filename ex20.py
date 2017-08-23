fcad15f47046eaa3e0a611952ac6c50660ef0257
Andrey <andryush333@gmail.com>
2017-08-23 13:09:27 +0400

    nothing special

--- ex20.py
+++ ex20.py
@@ -5,7 +5,7 @@ def print_all(f): # f is variable and whatever we can pass
     print(f.read()) # reading file
 
 def rewind(f):
-    f.seek(0) # seek is for move coursore to begin of the file
+    f.seek(0) # seek is for move coursore to begin of the file 0-is a byte
 
 def print_a_line(line_count, f): #line_count is also variable and anything other can pass there
     print(line_count, f.readline()) #readline is reading current line, when print is nothing it means end of file
